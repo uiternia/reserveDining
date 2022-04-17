5ad117615148e5446b1a3b91acbf8c39e90d0d46
yuusuke <uiternia0910@gmail.com>
2022-04-17 19:23:25 +0900

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
     }
 }
